7c44a6f05cc16491e33fc5fd411be28bcc378cc6
Ward Price <wardprice@gmail.com>
2020-06-27 17:06:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,17 @@
 def using_concat(array1, array2)
   array1.concat(array2)
 end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete
